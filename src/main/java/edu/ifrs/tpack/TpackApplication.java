de4a2bed9b0264be0b6ec46a591c1cd16e931de4
limaflores <limaflores@gmail.com>
2021-10-11 23:51:20 -0300

    Corrigido comentários

--- src/main/java/edu/ifrs/tpack/TpackApplication.java
+++ src/main/java/edu/ifrs/tpack/TpackApplication.java
@@ -25,6 +25,9 @@ import javax.ws.rs.core.Application;
 import edu.ifrs.tpack.service.Controller;
 
 @ApplicationPath("/service")
+/**
+*Comentário
+**/
 public class TpackApplication extends Application {
 
     @Override
